e2fefc665aa8b9bf93650645834e9a5ca088f391
Joseph_Berro <berr0107@algonquincollege.com>
2019-09-23 19:50:10 -0400

    Initial commit

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -18,52 +18,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
 
-        Button btn = (Button)findViewById(R.id.button1);
-        if(btn != null)
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.i( "Message", "You clicked button 1");
-                    btn.setText("You got me");
-
-                }
-            });
-
-        CheckBox btn2 = (CheckBox)findViewById(R.id.checkBox);
-        btn2.setOnCheckedChangeListener((buttonView, isChecked)-> {
-
-                    boolean newvalue = isChecked;
-
-                });
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//
-//            }
-//        });
-
-//            Log.i("Message", "You clicked checkBox");
-//            btn2.setText("you clicked button 2");
-//                });
-
-
-        // initiate a Switch
-        Switch simpleSwitch = (Switch) findViewById(R.id.switchButton);
-        // check current state of a Switch (true or false).
-        Boolean switchState = simpleSwitch.isChecked();
-        simpleSwitch.setChecked(true);
-//        simpleSwitch.setTextOn("On"); // displayed text of the Switch whenever it is in checked or on state
-//        simpleSwitch.setTextOff("Off"); // displayed text of the Switch whenever it is in unchecked i.e. off state
-
-        EditText input = findViewById(R.id.textView2);
-
-        ImageButton bt3 = (ImageButton)findViewById(R.id.imageButton);
-        bt3.setOnClickListener(clk -> {
-
-            String answer = input.getText().toString();
-            Log.i("Message", "You typed:" + answer);
-        });
     }
 }
